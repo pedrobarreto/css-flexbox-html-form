e9578c61f061bcb989f883f149443e70547614f0
MuriloM <muriloommaia@gmail.com>
2021-07-21 18:43:11 -0300

    ok

--- murilo.js
+++ murilo.js
@@ -1,8 +1,12 @@
 // requisito 18
+const agree = document.getElementById('agreement');
 const submitBtn = document.getElementById('submit-btn');
-submitBtn.addEventListener('click', (event) => {
-  const agree = document.getElementById('agreement');
-  if (!agree.checked) {
-    event.preventDefault();
+agree.addEventListener('change', () => {
+  if(agree.checked) {
+    submitBtn.disabled = false;
   }
-});
+  else {
+    submitBtn.disabled = true;
+  }
+})
+
