b1e0974964fe5ca1c03042a1d70d16e0e67b9066
Pedro Barreto <pbdeoliveira@gmail.com>
2021-07-21 17:54:51 -0300

    Requisito 14

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 let logarButton = document.querySelector('.submit-button');
 let pwdInput = document.querySelector('[name=pwd]');
-let LoginInput = document.querySelector('[name=login]')
-
+let loginInput = document.querySelector('[name=login]')
 function formSubmit(event) {
   event.preventDefault();
-  if( pwdInput.value === '123456' && LoginInput.value === 'tryber@teste.com') { 
+  if( pwdInput.value === '123456' && loginInput.value === 'tryber@teste.com') { 
    window.alert('Olá, Tryber!')
   }
   window.alert('Login ou senha inválidos.')
 }
-
 logarButton.addEventListener('click', formSubmit);
+
+function trybeRating() {
+  let labelRate = document.querySelector('#trybe-rating');
+  for ( i=0; i < 10; i += 1) {
+  let createElement = document.createElement('input')  
+  labelRate.appendChild(createElement).innerText = [i];
+  labelRate.appendChild(createElement).type = 'radio';
+  labelRate.appendChild(createElement).name = 'rate';
+  labelRate.appendChild(createElement).value = [i];
+  }
+  }
+  
+  window.onload = function() {
+    trybeRating();
+    };
+    
