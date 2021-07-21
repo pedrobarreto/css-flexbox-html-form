4a3beab01925e1f4012e89529455bc595d2f6970
MuriloM <muriloommaia@gmail.com>
2021-07-21 18:32:08 -0300

    requisitos 14, 16, 17, 19

--- script.js
+++ script.js
@@ -24,6 +24,15 @@ function trybeRating() {
   }
   }
   
+  // requisito 18
+const submitBtn = document.getElementById('submit-btn');
+submitBtn.addEventListener('click', (event) => {
+  const agree = document.getElementById('agreement');
+  if (!agree.checked) {
+    event.preventDefault();
+  }
+});
+
   window.onload = function() {
     trybeRating();
     };
