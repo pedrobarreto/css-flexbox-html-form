a6cb9641d44055c8a65dc5c6fa9c09857ee556ae
Pedro Barreto <pbdeoliveira@gmail.com>
2021-07-21 18:27:50 -0300

    Requisito 14 final

--- script.js
+++ script.js
@@ -11,13 +11,16 @@ function formSubmit(event) {
 logarButton.addEventListener('click', formSubmit);
 
 function trybeRating() {
-  let labelRate = document.querySelector('#label-rate');
-  for ( i=0; i < 10; i += 1) {
+  let labelRate = document.querySelector('#trybe-rating');
+  for ( i=1; i <= 10; i += 1) {
+  let createLabel = document.createElement('label')  
   let createElement = document.createElement('input')  
-  labelRate.appendChild(createElement).innerText = [i];
   labelRate.appendChild(createElement).type = 'radio';
   labelRate.appendChild(createElement).name = 'rate';
   labelRate.appendChild(createElement).value = [i];
+  labelRate.appendChild(createElement).id = [i]
+  createLabel.for=[i];
+  labelRate.appendChild(createLabel).innerText = [i];
   }
   }
   
